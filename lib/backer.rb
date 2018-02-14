f039de2f10159510864b043e5a5c27be646fb5b9
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-02-14 12:25:26 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,12 +1,12 @@
 class Backer
-attr_accessor :name, :backed_project
+attr_accessor :name, :backed_projects
 
   def initialize(name)
     @name = name
-    @backed_project = []
+    @backed_projects = []
   end
 
   def back_project(project)
-    @backed_project << project
+    @backed_projects << project
   end
 end
